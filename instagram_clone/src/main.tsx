33fe1be9d6e35fedfa52065b3334901049d74c06
Fran Miele <162506996+BlueBoi26@users.noreply.github.com>
2024-05-23 15:52:38 -0500

    fixed main

--- instagram_clone/src/main.tsx
+++ instagram_clone/src/main.tsx
@@ -4,7 +4,7 @@ import App from "./App.tsx";
 import "./style.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
